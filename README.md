18cc933d26ed67cce9abb3805536f96c5d367767
Feng-Zihao <feng.zh.zq@gmail.com>
2016-07-02 01:40:17 +0800

    capture arrow up/down

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ console GUI tool to manage ssh
 
 Useful Link to implement
 ---
-
-
 [cursor movement](http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x361.html)
-[ncurses]()
+
+[ncurses](http://tldp.org/HOWTO/NCURSES-Programming-HOWTO/)
--- main.cpp
+++ main.cpp
@@ -1,29 +1,104 @@
-#include <stdio.h>
+#include <cstdio>
 #include <unistd.h>
+#include <termios.h>
+
+void initKeyboard();
+
+void restoreKeyboard();
+
+static struct termios old_tio, new_tio;
+
+
+class Window {
+private:
+    int const rowSize;
+    int currentRow;
+
+    void renderSingleRow(int n) {
+
+    }
+
+    void clearSingleRow(int n) {
+
+    }
+
+    void moveCursorUp(int n) {
+
+    }
+
+    void moveCursorToBegining() {
+
+    }
+
+
+public:
+    Window(int row) : rowSize(row) { }
+
+    void render() {
+        moveCursorUp(0);
+    }
+
+    void getTerminalWidth() {
+
+    }
+
+};
+
 
 int main() {
+    initKeyboard();
+    int c;
 
+    do {
+        c = getchar();
+        printf("%d\n", c);
 
-    //write some input
-    printf("hello\nworld\n");
-    fflush(stdout);
+        // handle arrow key
+        if (c == 27) {
+            getchar();
 
-    //wait one second to change line above
-    sleep(1);
+            c = getchar();
+            switch (c) {
+                case 65:
+                    printf("key up\n");
+                    break;
+                case 66:
+                    printf("key down\n");
+                    break;
+                case 68:
+                    printf("key left\n");
+                    break;
+                case 67:
+                    printf("key right\n");
+                    break;
+                default:
+                    // not handled
+                    break;
+            }
 
-    // move up 1 line
-    printf("\033[1A222");
+        }
+    } while (c != 'q');
 
-    //remove line
-//    printf("\b");
-    fflush(stdout);
+    restoreKeyboard();
 
-    sleep(1);
-//    fputs("\033[A\033[2K", stdout);
-//    rewind(stdout);
-
-    //write new line
-    fputs("bye\n", stdout);
 
     return 0;
+}
+
+void initKeyboard() {
+    /* get the terminal settings for stdin */
+    tcgetattr(STDIN_FILENO, &old_tio);
+    /* we want to keep the old setting to restore them a the end */
+    new_tio = old_tio;
+    /* disable canonical mode (buffered i/o) and local echo */
+    new_tio.c_lflag &= (~ICANON & ~ECHO);
+    /* set the new settings immediately */
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+
+}
+
+void restoreKeyboard() {
+    /* restore the former settings */
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+
 }
